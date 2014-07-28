76a6bf0215fbb8dd99b818b8c191e724119129e6
timthornton <tim.thornton@bedarra.com>
2014-07-27 21:48:06 -0400

    Bounds checking

--- Chess/validator.hs
+++ Chess/validator.hs
@@ -5,4 +5,10 @@ import Chess.Types
 isValid (from, Just (sourceColor, Pawn)) to board = False
 isValid source@(from, piece) to board = True
 
+checkBounds (x, y) | x >= 0 && x < 8 && y >= 0 && y < 8 = True
+checkBounds _ = False
+
+getForward Black (x, y) = (x, y + 1)
+getForward Black (x, y) = (x, y - 1)
+
 
