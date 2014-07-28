03f4a78d18f01616922a56ce8f84aa03e2139c42
timthornton <tim.thornton@bedarra.com>
2014-07-27 21:53:27 -0400

    Add adjacent

--- Chess/validator.hs
+++ Chess/validator.hs
@@ -1,4 +1,4 @@
-module Chess.Validator where
+module Chess.Validator (isValid) where
 
 import Chess.Types
 
@@ -9,6 +9,10 @@ checkBounds (x, y) | x >= 0 && x < 8 && y >= 0 && y < 8 = True
 checkBounds _ = False
 
 getForward Black (x, y) = (x, y + 1)
-getForward Black (x, y) = (x, y - 1)
+getForward White (x, y) = (x, y - 1)
+getBackward c l = getForward (nextColor c) l
+getLeft  (x, y) = (x - 1, y)
+getRight (x, y) = (x + 1, y)
 
+getAdjacent l = [getLeft l, getForward Black l, getRight l, getBackward Black l]
 
